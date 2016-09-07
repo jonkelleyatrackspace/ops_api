926af70ee98e9d2a5c77c182dd19a82532ae96a2
jon <jon.kelley@rackspace.com>
2016-09-06 19:58:12 -0500

    Renaming sdk stuff

--- opsapi/sdk.py
+++ opsapi/sdk.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# vi: set ft=python :
 # file: sdk.py
-# Copyright 2016, Jonathan Kelley
-#
-# License: the mit license
-#    Permission is hereby granted, free of charge, to any person obtaining a copy of
-#    this software and associated documentation files (the "Software"), to deal in
-#    the Software without restriction, including without limitation the rights to
-#    use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-#    the Software, and to permit persons to whom the Software is furnished to do so,
-#    subject to the above copyright notice and this permission notice shall being included
-#    in all copies or substantial portions of the Software.
+# ---
+# © 2016, Jonathan Kelley <jon@uberleet.org>
+# Underwritten by The MIT License:
+#    Permission is hereby granted,  free of charge,  to any person obtaining 
+#    a copy of this software and associated documentation files  (the "Soft-
+#    ware"), to deal in the Software without restriction,  including without
+#    limitation the rights to use,  copy, modify, merge, publish, distribute
+#    , sublicense, and/or sell copies of the Software, and to permit persons
+#    to whom the Software is furnished to do so,  subject to the above copy-
+#    right notice and this permission notice shall being included in all 
+#    copies or substantial portions of the Software.
+#    NOTE: Full terms in `LICENSE` by setuptools distribution or git.
 
 from __future__ import (print_function, absolute_import)
 from os import environ
 import inspect  # used by compile_parameters()
 import sys
 
-from constants import Constants
-
+from .constants import Constants
 
 """
 This is the Source Development Kit (SDK) for OpsAPI
@@ -30,18 +30,6 @@ CORE extensions available to opsapi. You can utilize this SDK in your own extens
 by accessing classes, functions and methods. Private methods/functions will start with
 an underscore, and should NOT be called (they may change between "official" versions.
 """
-
-# This SDK is subject to change. If you modify a public function, classname or method
-# then look at what you should mark this change as below.
-# - New Classes/Methods           = Minor Release
-# - Rename/Delete Classes/Methods = Major Release
-# The Tornado app will build a collection of extensions and check sdk compatability
-# against the version below. If the version is not compatible then the extension
-# will be excluded from the import into OPSAPI and a log message generated.
-#
-# Extensions will be able to use > < macros in their definitions to take advantage
-# of known specifics. Format is SDK_VERSION = "M.m" (M=major,m=minor)
-SDK_VERSION = "2.0"
 
 
 def validate_parameters(cls_collection):
